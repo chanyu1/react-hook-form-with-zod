38c5e043f618337c32b9815e440796eec240a802
chanyu1 <haneul1ij@gmail.com>
2023-05-02 00:19:47 +0900

    feat: react hook form with zod

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { validationSchema } from './utils/validationSchema';
 import './App.css';
 
+interface LoginForm {
+  name: string;
+  email: string;
+  password: string;
+}
+
 function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginForm>({
+    mode: 'onChange',
+    resolver: zodResolver(validationSchema),
+  });
+
+  const onSubmit = (data: LoginForm) => {
+    console.log(data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="form-container">
+      <h1>Login Form</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="name">Name</label>
+        <input id="name" type="text" {...register('name')} />
+        <p>{errors.name?.message as React.ReactNode}</p>
+        <label htmlFor="email">Email</label>
+        <input id="email" type="email" {...register('email')} />
+        <p>{errors.email?.message as React.ReactNode}</p>
+        <label htmlFor="password">Password</label>
+        <input id="password" type="password" {...register('password')} />
+        <p>{errors.password?.message as React.ReactNode}</p>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
---
+++ src/utils/validationSchema.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+
+export const validationSchema = z.object({
+  name: z
+    .string()
+    .nonempty('Name is required')
+    .min(4, 'Enter at least 4 characters'),
+  email: z.string().nonempty('Email is required').email('Invalid email format'),
+  password: z
+    .string()
+    .nonempty('Password is required')
+    .min(6, 'Enter at least 6 characters'),
+});
